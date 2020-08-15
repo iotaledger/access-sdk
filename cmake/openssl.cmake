f43da4d3355fb3863c985adcaa12e913e8c22c90
bernardo <bernardoaraujor@gmail.com>
2020-08-15 19:51:59 +0200

    fetch OpenSSL 1.0.2 tarball

--- cmake/openssl.cmake
+++ cmake/openssl.cmake
@@ -1,10 +1,31 @@
-# fetch openssl
-include(FetchContent)
-FetchContent_Declare(
-  openssl
-  GIT_REPOSITORY https://github.com/honeycombOS/openssl-cmake.git
-  GIT_TAG 8ffd9276ec8750ae4293cfa0bc9e7562807bb37d
+include(ExternalProject)
+
+set(OPENSSL_VERSION 1.0.2)
+set(OPENSSL_TARBALL OpenSSL_1_0_2u.tar.gz)
+set(OPENSSL_URL https://github.com/openssl/openssl/archive/${OPENSSL_TARBALL})
+set(OPENSSL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/src/openssl-OpenSSL_1_0_2u)
+set(OPENSSL_STATIC_SSL_LIB ${CMAKE_CURRENT_BINARY_DIR}/lib/libssl.a)
+set(OPENSSL_STATIC_CRYPTO_LIB ${CMAKE_CURRENT_BINARY_DIR}/lib/libcrypto.a)
+set(OPENSSL_INCLUDES ${CMAKE_INSTALL_PREFIX}/include)
+
+file(MAKE_DIRECTORY ${OPENSSL_INCLUDES})
+
+ExternalProject_Add(
+    openssl
+    PREFIX ${OPENSSL_PREFIX}
+    BUILD_IN_SOURCE 1
+    DOWNLOAD_COMMAND wget ${OPENSSL_URL} && tar xvf ${OPENSSL_TARBALL}
+    CONFIGURE_COMMAND cd ${OPENSSL_PREFIX} && ./config --prefix=${CMAKE_CURRENT_BINARY_DIR} --openssldir=${OPENSSL_PREFIX}
+    BUILD_COMMAND cd ${OPENSSL_PREFIX} && make
+    INSTALL_COMMAND cd ${OPENSSL_PREFIX} && make install
+    BUILD_BYPRODUCTS ${OPENSSL_STATIC_CRYPTO_LIB} ${OPENSSL_STATIC_SSL_LIB}
 )
 
-message(STATUS "Fetching openssl")
-FetchContent_MakeAvailable(openssl)
+add_library(ssl STATIC IMPORTED GLOBAL)
+add_library(crypto STATIC IMPORTED GLOBAL)
+
+set_target_properties(ssl PROPERTIES IMPORTED_LOCATION ${OPENSSL_STATIC_SSL_LIB})
+set_target_properties(ssl PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDES})
+
+set_target_properties(crypto PROPERTIES IMPORTED_LOCATION ${OPENSSL_STATIC_CRYPTO_LIB})
+set_target_properties(crypto PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDES})
