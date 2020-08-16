da46845cd7d96e9087b646a948781a72f52a779e
bernardo <bernardoaraujor@gmail.com>
2020-08-16 02:25:14 +0200

    fix libsodium.cmake

--- cmake/libsodium.cmake
+++ cmake/libsodium.cmake
@@ -4,13 +4,13 @@ set(SODIUM_VERSION 1.0.18)
 set(SODIUM_TARBALL libsodium-${SODIUM_VERSION}.tar.gz)
 set(SODIUM_URL https://download.libsodium.org/libsodium/releases/${SODIUM_TARBALL})
 set(SODIUM_SRC ${CMAKE_CURRENT_BINARY_DIR}/src/libsodium-${SODIUM_VERSION})
-set(SODIUM_STATIC_LIB ${CMAKE_INSTALL_PREFIX}/lib/libsodium.a)
-set(SODIUM_INCLUDES ${CMAKE_INSTALL_PREFIX}/include)
+set(SODIUM_STATIC_LIB ${CMAKE_CURRENT_BINARY_DIR}/lib/libsodium.a)
+set(SODIUM_INCLUDES ${CMAKE_CURRENT_BINARY_DIR}/include)
 
 file(MAKE_DIRECTORY ${SODIUM_INCLUDES})
 
 ExternalProject_Add(
-    libsodium
+    sodium-src
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}
     SOURCE_DIR ${SODIUM_SRC}
     DOWNLOAD_COMMAND wget ${SODIUM_URL} && tar xvf ${SODIUM_TARBALL} && ${SODIUM_SRC}/autogen.sh
@@ -22,7 +22,7 @@ ExternalProject_Add(
 
 add_library(sodium STATIC IMPORTED GLOBAL)
 
-add_dependencies(sodium libsodium)
+add_dependencies(sodium sodium-src)
 
 set_target_properties(sodium PROPERTIES IMPORTED_LOCATION ${SODIUM_STATIC_LIB})
 set_target_properties(sodium PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${SODIUM_INCLUDES})
