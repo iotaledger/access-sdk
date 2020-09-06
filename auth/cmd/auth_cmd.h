3dfc4c68506948fc0196bf8511cc4cb8929bc2ba
bernardo <bernardoaraujor@gmail.com>
2020-09-06 16:56:56 +0200

    bootstrap auth_cmd

--- auth/cmd/auth_cmd.h
+++ auth/cmd/auth_cmd.h
@@ -1,11 +1,11 @@
-#include "pdp.h"
+#include "policy.h"
 
 #ifndef AUTH_CMD_H
 #define AUTH_CMD_H
 
 typedef struct {
   char *policy_id;
-  pdp_action_t action;
+  action_t action;
 } auth_cmd_t;
 
 #endif //AUTH_CMD_H
